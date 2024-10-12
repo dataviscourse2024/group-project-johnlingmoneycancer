6a52fb862413e8906c00d2ef037e467e70b938e7
mmoneymike <u0712166@utah.edu>
2024-10-12 14:40:16 -0600

    Added basic foundation for webpage and visualization

--- js/cancer-visualization.js
+++ js/cancer-visualization.js
@@ -1,1 +1,5 @@
- print
+ // Select the visualization div and append an SVG element
+const svg = d3.select("#visualization")  // This targets the <div id="visualization"> in your HTML
+.append("svg")
+.attr("width", 1000)
+.attr("height", 600);
